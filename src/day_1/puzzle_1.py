41484b58fe341889554be292bc7e21dee80ec6c8
Sebastian Graf <sebastian.graf@rheinmetall.com>
2025-12-01 17:47:00 +0100

    Puzzle Day 1.1 solved

--- src/day_1/puzzle_1.py
+++ src/day_1/puzzle_1.py
@@ -1,7 +1,55 @@
 from src.utils.input_reader import read_input
+from typing import Tuple
+
+
+class DialSolver:
+
+    def __init__(self, start_position: int = 50, max_value: int = 99):
+        self.position = start_position
+        self.amount_values = max_value + 1
+        self.counter_zero = 0
+
+    def process_inputs(self, inputs: list[str]):
+        for s in inputs:
+            self._process_input(s)
+
+    def _process_input(self, s: str):
+        letter, digit = self._split_letter_number(s)
+
+        if letter == "L":
+            self.position = (self.position - digit) % self.amount_values
+
+        if letter == "R":
+            self.position = (self.position + digit) % self.amount_values
+
+        if self.position == 0:
+            self.counter_zero += 1
+
+    @staticmethod
+    def _split_letter_number(s: str) -> Tuple[str, int]:
+        if s is None:
+            raise ValueError("Input must be a string")
+        s = s.strip()
+        if len(s) < 2:
+            raise ValueError("Input too short; expected at least 2 characters")
+
+        letter, digit = s[0], s[1:]
+        if letter not in ["L", "R"]:
+            raise ValueError("First character must either `L` or `R`")
+        if not digit.isdigit():
+            raise ValueError("Remaining characters must be a digit")
+
+        return letter, int(digit)
+
+    def print_solution(self):
+        print(f"The dial point {self.counter_zero} times to zero.")
+
 
 def main():
-    print(read_input(1,1))
+    dial_solver = DialSolver()
+    dial_solver.process_inputs(read_input(1, 1))
+    dial_solver.print_solution()
+
 
 if __name__ == "__main__":
     main()
