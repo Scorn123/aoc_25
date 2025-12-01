ce0acebea13c4149b65f2d83fcd6115e2994ea3f
Sebastian Graf <sebastian.graf@rheinmetall.com>
2025-12-01 18:13:40 +0100

    WIP

--- src/day_1/puzzle_1.py
+++ src/day_1/puzzle_1.py
@@ -7,7 +7,8 @@ class DialSolver:
     def __init__(self, start_position: int = 50, max_value: int = 99):
         self.position = start_position
         self.amount_values = max_value + 1
-        self.counter_zero = 0
+        self.counter_zero_landed = 0
+        self.counter_zero_passed = 0
 
     def process_inputs(self, inputs: list[str]):
         for s in inputs:
@@ -17,13 +18,26 @@ class DialSolver:
         letter, digit = self._split_letter_number(s)
 
         if letter == "L":
-            self.position = (self.position - digit) % self.amount_values
+            total_wraps = (digit - self.position + self.amount_values - 1) // self.amount_values
+
+            self.position -= digit
+            self.position %= self.amount_values
+
+
+            self.counter_zero_passed += total_wraps
+
+            if self.position == 0:
+                self.counter_zero_landed += 1
 
         if letter == "R":
-            self.position = (self.position + digit) % self.amount_values
+            total_wraps = (self.position + digit) // self.amount_values
+            self.position += digit
+            self.position %= self.amount_values
 
-        if self.position == 0:
-            self.counter_zero += 1
+            if self.position == 0:
+                self.counter_zero_landed += 1
+
+            self.counter_zero_passed += total_wraps
 
     @staticmethod
     def _split_letter_number(s: str) -> Tuple[str, int]:
@@ -41,14 +55,19 @@ class DialSolver:
 
         return letter, int(digit)
 
-    def print_solution(self):
-        print(f"The dial point {self.counter_zero} times to zero.")
+    def print_solution_part_1(self):
+        print(f"The dial point {self.counter_zero_landed} times to zero.")
+
+    def print_solution_part_2(self):
+        print(f"The dial passes and lands {self.counter_zero_passed}"
+              f" times zero.")
 
 
 def main():
     dial_solver = DialSolver()
     dial_solver.process_inputs(read_input(1, 1))
-    dial_solver.print_solution()
+    dial_solver.print_solution_part_1()
+    dial_solver.print_solution_part_2()
 
 
 if __name__ == "__main__":
